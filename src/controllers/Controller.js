88b6e8c2b086afa0dbb0dcf8e6e5456bc5b4295b
André Lucas <andrelucca99@gmail.com>
2024-06-18 12:47:26 -0300

    feat: add tratativa de erros nas controllers

--- src/controllers/Controller.js
+++ src/controllers/Controller.js
@@ -8,7 +8,7 @@ class Controller {
       const listaDeRegistro = await this.entidadeService.pegaTodosOsRegistros();
       return res.status(200).json(listaDeRegistro);
     } catch (error) {
-      return res.status(400).json(error.message);
+      return res.status(500).json({ erro: error.message });
     }
   }
 
@@ -18,7 +18,7 @@ class Controller {
       const umRegistro = await this.entidadeService.pegaUmRegistroPorId(Number(id));
       return res.status(200).json(umRegistro);
     } catch (error) {
-      return res.status(400).json(error.message);
+      return res.status(500).json({ erro: error.message });
     }
   }
 
@@ -28,7 +28,7 @@ class Controller {
       const novoRegistroCriado = await this.entidadeService.criaRegistro(dadosParaCriacao);
       return res.status(200).json(novoRegistroCriado);
     } catch (error) {
-      return res.status(400).json(error.message);
+      return res.status(500).json({ erro: error.message });
     }
   }
 
@@ -36,14 +36,13 @@ class Controller {
     const { id } = req.params;
     const dadosAtualizados = req.body;
     try {
-      //isUpdated
       const foiAtualizado = await this.entidadeService.atualizaRegistro(dadosAtualizados, Number(id));
       if (!foiAtualizado) {
         return res.status(400).json({ mensagem: 'registro não foi atualizado' });
       }
       return res.status(200).json({ mensagem: 'Atualizado com sucesso' });
     } catch (error) {
-      return res.status(400).json(error.message);
+      return res.status(500).json({ erro: error.message });
     }
   }
 
@@ -55,7 +54,7 @@ class Controller {
 
 
     } catch (error) {
-      return res.status(500).json(error.message);
+      return res.status(500).json({ erro: error.message });
     }
   }
 }
